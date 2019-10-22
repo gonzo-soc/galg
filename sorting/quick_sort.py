c18034af0c4ce029bbce67822cc62d7f0d9c8db2
Gonzo-soc <gonzo.soc@gmail.com>
2019-10-23 03:27:59 +1100

    Quick sort implementation

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -5,7 +5,7 @@ Chapter 04 - 1. Quick sort implementation (O(n * log(n)))
 '''
 
 
-def quick_sort(in_array, left, right):
+def quick_sort_SORT_GALG(in_array, left, right):
     if len(in_array) < 2:
 
         print("Debug quick_sort: return array is less then 2 in length")
@@ -16,9 +16,9 @@ def quick_sort(in_array, left, right):
     sn_ind = int((left + right)/2)
     sn = in_array[sn_ind]
 
-    less_stable_nodes = filterLessStableNode(in_array, sn)
-    higher_stable_nodes = filterHigherStableNode(in_array, sn)
-    equal_stable_nodes = filterEqualStableNode(in_array, sn)
+    less_stable_nodes = filterLessStableNode_SORT_GALG(in_array, sn)
+    higher_stable_nodes = filterHigherStableNode_SORT_GALG(in_array, sn)
+    equal_stable_nodes = filterEqualStableNode_SORT_GALG(in_array, sn)
 
     print('Debug quick_sort: less_stable_nodes')
     print(less_stable_nodes)
@@ -32,23 +32,23 @@ def quick_sort(in_array, left, right):
     (although we don't apply return to the function call directly
     - we use concatenation - "+")
     '''
-    return quick_sort(
+    return quick_sort_SORT_GALG(
         less_stable_nodes,
         0, len(less_stable_nodes)
-    ) + equal_stable_nodes + quick_sort(
+    ) + equal_stable_nodes + quick_sort_SORT_GALG(
         higher_stable_nodes, 0, len(higher_stable_nodes)
     )
 
 
-def filterLessStableNode(in_array, sn):
+def filterLessStableNode_SORT_GALG(in_array, sn):
     return [i for i in in_array[0:] if i < sn]
 
 
-def filterHigherStableNode(in_array, sn):
+def filterHigherStableNode_SORT_GALG(in_array, sn):
     return [i for i in in_array[0:] if i > sn]
 
 
-def filterEqualStableNode(in_array, sn):
+def filterEqualStableNode_SORT_GALG(in_array, sn):
     return [i for i in in_array[0:] if i == sn]
 
 
@@ -59,7 +59,7 @@ in_array = [3, 2, 1]
 print(in_array)
 
 out_array = []
-sorted_array = quick_sort(in_array, 0, len(in_array))
+sorted_array = quick_sort_SORT_GALG(in_array, 0, len(in_array))
 
 print('Debug: Sorted_array:\n')
 print(sorted_array)
@@ -72,7 +72,7 @@ in_array = [5, 4, 3, 2, 1]
 print(in_array)
 
 out_array = []
-sorted_array = quick_sort(in_array, 0, len(in_array))
+sorted_array = quick_sort_SORT_GALG(in_array, 0, len(in_array))
 
 print('Debug: Sorted_array:\n')
 print(sorted_array)
@@ -86,7 +86,7 @@ in_array = [5, 5, 4, 3, 2, 4, 1]
 print(in_array)
 
 out_array = []
-sorted_array = quick_sort(in_array, 0, len(in_array))
+sorted_array = quick_sort_SORT_GALG(in_array, 0, len(in_array))
 
 print('Debug: Sorted_array:\n')
 print(sorted_array)
@@ -100,7 +100,7 @@ in_array = [1, 2, 3, 4, 5, 6, 7]
 print(in_array)
 
 out_array = []
-sorted_array = quick_sort(in_array, 0, len(in_array))
+sorted_array = quick_sort_SORT_GALG(in_array, 0, len(in_array))
 
 print('Debug: Sorted_array:\n')
 print(sorted_array)
@@ -114,7 +114,7 @@ in_array = [-9, 0, 1, -1, 2, 8, 10, -10, -20, -15]
 print(in_array)
 
 out_array = []
-sorted_array = quick_sort(in_array, 0, len(in_array))
+sorted_array = quick_sort_SORT_GALG(in_array, 0, len(in_array))
 
 print('Debug: Sorted_array:\n')
 print(sorted_array)
